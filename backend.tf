da885ca80a650a873ce7b4433c62cc522daf3524
surendra1302 <surendrakorivi96@gmail.com>
2025-03-09 23:47:16 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket         = "terraform-jenkins-ansible"
     key            = "ec2/terraform.tfstate"
     region         = "us-east-1"
-//    dynamodb_table = "terraform-lock"
+    dynamodb_table = "terraform-lock"
   }
 }
